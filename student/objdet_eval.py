2e881ab5896f55fa627984bf388de5c2e52b737d
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2022-05-29 19:35:26 -0400

    Fix bugs for computing IoU

--- student/objdet_eval.py
+++ student/objdet_eval.py
@@ -42,55 +42,116 @@ def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5)
         matches_lab_det = []
         if valid: # exclude all labels from statistics which are not considered valid
             
+            #     ## step 1 : extract the four corners of the current label bounding-box
+            # # x y w l yaw for compute_box_corners
+            # lx = label.box.center_x
+            # ly = label.box.center_y
+            # lz = label.box.center_z
+            # label_corners = tools.compute_box_corners (lx, ly, label.box.width, label.box.length, label.box.heading)
+            # label_area = Polygon(label_corners)
+            
+            ## step 2 : loop over all detected objects
             for det in detections:
-                box = label.box
-                rx = box.center_x + box.width/2
-                lx = box.center_x - box.width/2
-                ty = box.center_y - box.length/2
-                by = box.center_y + box.length/2
-                tz = box.center_z + box.height/2
-                bz = box.center_z - box.height/2
-                tr, tl, bl, br = (rx, ty), (lx, ty), (lx, by), (rx, by)
-                gt_rect = Polygon([bl, br, tr, tl])
-                # _, bev_x, bev_y, z, h, bev_w, bev_l, yaw = det
-                _, x, y, z, w, l, h, _ = det
-                det_lx = x
-                det_rx = x + w
-                det_ty = y
-                det_by = y + l
-                det_tr, det_tl, det_bl, det_br = (det_rx, det_ty), (det_lx, det_ty), (det_lx, det_by), (det_rx, det_by)
-                det_rect = Polygon([det_bl, det_br, det_tr, det_tl])
-
-                get_x_coord = lambda tup: tup[0]
-                get_y_coord = lambda tup: tup[1]
-                rightmost_left = max(bl, det_bl, key=get_x_coord)[0]
-                leftmost_right = min(br, det_br, key=get_x_coord)[0]
-                topmost_bottom = min(bl, det_bl, key=get_y_coord)[1]
-                bottommost_top = max(tl, det_tl, key=get_y_coord)[1]
-                iou_tr, iou_tl, iou_bl, iou_br = (leftmost_right, bottommost_top), (rightmost_left, bottommost_top), (rightmost_left, topmost_bottom), (leftmost_right, topmost_bottom)
+                # _, x, y, z, _, w, l, yaw = det
+                # ## step 3 : extract the four corners of the current detection
+                # det_corners = tools.compute_box_corners (x, y, w, l, yaw)
+                # det_area = Polygon(det_corners)
                 
-                import cv2
-                img = np.zeros((70, 70, 3))
-                print([tr, tl, bl, br])
-                print([det_tr, det_tl, det_bl, det_br])
-                print([iou_tr, iou_tl, iou_bl, iou_br])
-                img = cv2.polylines(img, [np.array([tr, tl, bl, br]).astype(np.int32)], isClosed=True, color=[0, 255, 0], thickness=2)
-                img = cv2.polylines(img, [np.array([det_tr, det_tl, det_bl, det_br]).astype(np.int32)], isClosed=True, color=[255, 0, 0], thickness=2)
-                color = [255, 255, 255] if leftmost_right-rightmost_left >= 0 and bottommost_top-topmost_bottom else [0, 0, 255]
-                img = cv2.polylines(img, [np.array([iou_tr, iou_tl, iou_bl, iou_br]).astype(np.int32)], isClosed=True, color=color, thickness=1)
+                # ## step 4 : compute the center distance between label and detection bounding-box in x, y, and z
+                # dist_x = lx - x
+                # dist_y = ly - y
+                # dist_z = lz - z
+                
+                # ## step 5 : compute the intersection over union (IOU) between label and detection bounding-box
+                # intersec = label_area.intersection(det_area)
+                # union = label_area.union(det_area)
+                # iou = intersec.area / union.area
+                # print("IOU", iou)
+                
+                # ## step 6 : if IOU exceeds min_iou threshold, store [iou,dist_x, dist_y, dist_z] in matches_lab_det and increase the TP count
+                # if (iou > min_iou):
+                #     matches_lab_det.append([iou, dist_x, dist_y, dist_z])
+                #     print("Matches_lab_det:", matches_lab_det)
 
-                if leftmost_right-rightmost_left >= 0 and bottommost_top-topmost_bottom:
-                    intersection = abs(iou_tr[0]-iou_tl[0]) * abs(iou_tr[1] - iou_br[1])
-                    union = abs(tr[0]-tl[0]) * abs(tr[1] - br[1]) + abs(det_tr[0]-det_tl[0]) * abs(det_tr[1] - det_br[1]) - intersection
-                    calc_intersection = gt_rect.intersection(det_rect)
-                    calc_union = gt_rect.union(det_rect)
-                    print("My intersection: ", intersection)
-                    print("Calculated intersection", calc_intersection.area)
-                    print("My union:", union)
-                    print("Calculated union:", calc_union.area)
+                #     true_positives += 1
+
+
+
+
+
+
+
+
+
+
+
+
+                box = label.box
+                # rx = box.center_x + box.width/2
+                # lx = box.center_x - box.width/2
+                # ty = box.center_y - box.length/2
+                # by = box.center_y + box.length/2
+                # tz = box.center_z + box.height/2
+                # bz = box.center_z - box.height/2
+                # tr, tl, bl, br = (rx, ty), (lx, ty), (lx, by), (rx, by)
+                # gt_rect = Polygon([bl, br, tr, tl])
+
+                gt_rect = Polygon(tools.compute_box_corners(box.center_x, box.center_y, box.width, box.length, box.heading))
+
+
+                # _, bev_x, bev_y, z, h, bev_w, bev_l, yaw = det
+                _, x, y, z, h, w, l, yaw = det
+                # x, y, w, l = 
+                # print("XYWL", x,y,w,l)
+                det_rect = Polygon(tools.compute_box_corners(x, y, w, l, yaw))
+                # print("Gt rect:", gt_rect)
+                # print("Det rect:", det_rect)
+                # det_cx = x
+                # det_cy = y
+                # det_cz = z
+                # det_lx = x - w/2
+                # det_rx = x + w/2
+                # det_ty = y - l/2
+                # det_by = y + l/2
+                # det_tr, det_tl, det_bl, det_br = (det_rx, det_ty), (det_lx, det_ty), (det_lx, det_by), (det_rx, det_by)
+                # det_rect = Polygon([det_bl, det_br, det_tr, det_tl])
+
+                # get_x_coord = lambda tup: tup[0]
+                # get_y_coord = lambda tup: tup[1]
+                # rightmost_left = max(bl, det_bl, key=get_x_coord)[0]
+                # leftmost_right = min(br, det_br, key=get_x_coord)[0]
+                # topmost_bottom = min(bl, det_bl, key=get_y_coord)[1]
+                # bottommost_top = max(tl, det_tl, key=get_y_coord)[1]
+                # iou_tr, iou_tl, iou_bl, iou_br = (leftmost_right, bottommost_top), (rightmost_left, bottommost_top), (rightmost_left, topmost_bottom), (leftmost_right, topmost_bottom)
+                
+                # import cv2
+                # img = np.zeros((70, 70, 3))
+                # print([tr, tl, bl, br])
+                # print([det_tr, det_tl, det_bl, det_br])
+                # print([iou_tr, iou_tl, iou_bl, iou_br])
+                # img = cv2.polylines(img, [np.array([tr, tl, bl, br]).astype(np.int32)], isClosed=True, color=[0, 255, 0], thickness=2)
+                # img = cv2.polylines(img, [np.array([det_tr, det_tl, det_bl, det_br]).astype(np.int32)], isClosed=True, color=[255, 0, 0], thickness=2)
+                # color = [255, 255, 255] if leftmost_right-rightmost_left >= 0 and bottommost_top-topmost_bottom else [0, 0, 255]
+                # img = cv2.polylines(img, [np.array([iou_tr, iou_tl, iou_bl, iou_br]).astype(np.int32)], isClosed=True, color=color, thickness=1)
+
+                # if leftmost_right-rightmost_left >= 0 and bottommost_top-topmost_bottom:
+                # intersection = abs(iou_tr[0]-iou_tl[0]) * abs(iou_tr[1] - iou_br[1])
+                # union = abs(tr[0]-tl[0]) * abs(tr[1] - br[1]) + abs(det_tr[0]-det_tl[0]) * abs(det_tr[1] - det_br[1]) - intersection
+                intersection = gt_rect.intersection(det_rect)
+                union = gt_rect.union(det_rect)
+                iou = intersection.area / union.area
+                print("IOU", iou)
+                if iou > min_iou:
+                    true_positives += 1
+                    matches_lab_det.append([iou, box.center_x - x, box.center_y - y, box.center_z - z])
+                    print("MATCHES_LAB_DET", matches_lab_det)
+                    # print("My intersection: ", intersection)
+                    # print("Calculated intersection", calc_intersection.area)
+                    # print("My union:", union)
+                    # print("Calculated union:", calc_union.area)
         
-                cv2.imshow("Im", img)
-                cv2.waitKey(0)
+                # cv2.imshow("Im", img)
+                # cv2.waitKey(0)
 
 
 
